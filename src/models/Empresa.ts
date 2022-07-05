eddf114b49ce52f98c4ae4cafbe348f53c09e66b
Alex Quispe <wilmer.quispe@agetic.gob.bo>
2022-07-05 10:40:10 -0400

    fix: eslint config

--- src/models/Empresa.ts
+++ src/models/Empresa.ts
@@ -1,8 +1,10 @@
 export class Empresa {
   razonSocial: string;
+
   constructor(razonSocial: string) {
     this.razonSocial = razonSocial;
   }
+
   mostrarInfo() {
     console.log("empresa: ", this.razonSocial);
   }
